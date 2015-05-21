8858064d9e87482c4037667b275b91065f8b4d64
ianpgo <ianpgo@gmail.com>
2015-05-21 17:58:52 -0400

    adds sessions controller create action

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -67,4 +67,9 @@ RSpec.configure do |config|
   config.before(:each, type: :controller) do
     include_default_accept_headers
   end
+
+  #Including to test requests
+  config.include Request::JsonHelpers, :type => :controller
+  config.include Request::HeadersHelpers, :type => :controller
+  config.include Devise::TestHelpers, :type => :controller
 end
