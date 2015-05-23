6cadf0b34e867707d5d7d824efc9e47e1e23cb0a
ianpgo <ianpgo@gmail.com>
2015-05-22 21:40:11 -0400

    Adds order confirmation mailer

--- Gemfile
+++ Gemfile
@@ -56,6 +56,7 @@ end
 group :test do
   gem "rspec-rails", "~> 2.14"
   gem 'shoulda-matchers'
+  gem "email_spec"
 end
 
 group :development, :test do
--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -14,10 +14,11 @@ class Api::V1::OrdersController < ApplicationController
 		order = current_user.orders.build(order_params)
 
 		if order.save
-			render json: order, status: 201, location: [:api, current_user, order]
-		else
-			render json: {errors: order.errors }, status: 422
-		end
+    		OrderMailer.send_confirmation(order).deliver
+    		render json: order, status: 201, location: [:api, current_user, order]
+  		else
+    		render json: { errors: order.errors }, status: 422
+  		end
 	end
 
 	private
