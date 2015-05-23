f4f9d9709416bd7b2b4ed48a94a524d5d60d4aaf
ianpgo <ianpgo@gmail.com>
2015-05-22 20:26:05 -0400

    Associates products and orders with a placements model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -16,6 +16,9 @@ describe Product do
 
   it { should belong_to :user}
 
+  it { should have_many(:placements) }
+  it { should have_many(:orders).through(:placements) }
+
   describe ".filter by title" do
     before(:each) do
       @product1 = FactoryGirl.create :product, title: "A plasma TV"
